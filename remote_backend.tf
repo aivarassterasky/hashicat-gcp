69e137cc392ad8a07a0bc37d51ddd3f9b7d19179
Your Name <you@example.com>
2021-11-01 12:09:52 +0000

    Added vpc module ..

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "example-org-fa4d68"
     workspaces {
       name = "hashicat-gcp"
--- vpc.tf
+++ vpc.tf
@@ -1,11 +1,13 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.4.0"
-subnets = [
-  {
-    subnet_name   = "gaurav-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
+  source       = "terraform-google-modules/network/google"
+  version      = "~> 3.4.0"
+  network_name = "gaurav-network"
+  project_id   = var.project
+  subnets = [
+    {
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
+      subnet_region = var.region
+    }
+  ]
 }
