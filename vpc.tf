75006c3eaa2320fbd75390a176d80d65f6ca859f
Your Name <you@example.com>
2021-11-01 12:29:56 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -4,8 +4,8 @@ module "vpc" {
   project_id   = var.project
   subnets = [
     {
-      subnet_name   = "gaurav1-subnet"
-      subnet_ip     = "10.101.10.0/24"
+      subnet_name   = "gaurav-subnet"
+      subnet_ip     = "10.100.10.0/24"
       subnet_region = var.region
     }
   ]
